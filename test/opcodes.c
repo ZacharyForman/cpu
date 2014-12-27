4b86a3bb6230ea78bf66db025913a47391da333d
Zachary Forman <au.zachary.forman@gmail.com>
2014-12-27 14:53:20 +1030

    Tests for I type instructions added.

--- test/opcodes.c
+++ test/opcodes.c
@@ -1,5 +1,6 @@
 // Tests for opcodes.h
 #include "../opcodes.h"
+#include "../types.h"
 #include "test.h"
 
 // Tests for R type instructions.
@@ -7,7 +8,7 @@ void test_r_opcodes()
 {
   int i;
   struct {
-    unsigned op;
+    word_t op;
     unsigned r_type;
     unsigned r_dst;
     unsigned r_src1;
@@ -38,12 +39,21 @@ void test_i_opcodes()
 {
   int i;
   struct {
-    unsigned op;
+    word_t op;
     unsigned type;
-    unsigned i_imm;
+    word_t i_imm;
     unsigned i_dst;
     unsigned i_src;
-  } t[0];
+  } t[] = {
+    {0x27E12710, ADDUI, 10000, 1, 31},
+    {0x24010000, ADDUI,     0, 1,  0},
+    {0x24210001, ADDUI,     1, 1,  1},
+    {0x3041000E,  ANDI,    14, 1,  2},
+    {0x3C01FFFF,   LHI, 65535, 1,  0},
+    {0x3C010000,   LHI,     0, 1,  0},
+    {0x53E10025,  SLAI,    37, 1, 31},
+    {0x50010005,  SLAI,     5, 1,  0}
+  };
 
   for (i = 0; i < sizeof(t)/sizeof(t[0]); i++) {
     REQUIRE(4*i, TYPE(t[i].op), t[i].type);
@@ -58,12 +68,17 @@ void test_i_s_opcodes()
 {
   int i;
   struct {
-    unsigned op;
+    word_t op;
     unsigned type;
-    signed i_imm;
+    sword_t i_imm;
     unsigned i_dst;
     unsigned i_src;
-  } t[0];
+  } t[] = {
+    {0x8C210004,   LW,     4,  1,  1},
+    {0x8FFF7FFF,   LW, 32767, 31, 31},
+    {0x93FFFFFF,  LBU,    -1, 31, 31},
+    {0x60210190, SEQI,   400,  1,  1}
+  };
 
   for (i = 0; i < sizeof(t)/sizeof(t[0]); i++) {
     REQUIRE(4*i, TYPE(t[i].op), t[i].type);
@@ -78,9 +93,9 @@ void test_l_opcodes()
 {
   int i;
   struct {
-    unsigned op;
+    word_t op;
     unsigned type;
-    unsigned l_imm;
+    word_t l_imm;
   } t[] = {
     {0x40000000,  RFE, 0},
     {0x44000002, TRAP, 2},
@@ -99,9 +114,9 @@ void test_l_s_opcodes()
 {
   int i;
   struct {
-    unsigned op;
+    word_t op;
     unsigned type;
-    signed l_imm;
+    sword_t l_imm;
   } t[] = {
     {0x0800000C,   J, 12},
     {0x0BFFFFF8,   J, -8},
