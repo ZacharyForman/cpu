75e8c44f8ab91557665dbf27d62263be60cd439e
ZacharyForman <au.zachary.forman@gmail.com>
2015-01-01 10:50:18 +1100

    Refactored disassembler to be less horrible

--- disassembler.c
+++ disassembler.c
@@ -7,33 +7,125 @@
 #include <stdio.h>
 #include <string.h>
 
-#define COMMAND_LEN 64
+// 24 maximum length of a command.
+// SNEUI\tr31, r31, -32768\0
+#define COMMAND_LEN 24
+
+// Parses a single hex instruction into its string representation.
+void parse_instr(char **lookup, char **r_lookup, 
+  char **special_reg_lookup, word_t instr, char *res)
+{
+  switch(TYPE(instr)) {
+    case 0: {
+      if (r_lookup[R_TYPE(instr)] == 0) {
+        sprintf(res, "%s", "UNKNOWN");
+      } else if (R_TYPE(instr) == MOVS2I) {
+        sprintf(res, "%s\t%d, %s",
+          lookup[R_TYPE(instr)],
+          R_DST(instr),
+          special_reg_lookup[R_SRC1(instr)]);
+      } else if (R_TYPE(instr) == MOVI2S) {
+        sprintf(res, "%s\t%s, %d",
+          lookup[R_TYPE(instr)],
+          special_reg_lookup[R_DST(instr)],
+          R_SRC1(instr));
+      } else if (instr == NOP ||
+          instr == HALT ||
+          instr == WAIT) {
+        sprintf(res, "%s", 
+          r_lookup[R_TYPE(instr)]);
+      } else {
+        sprintf(res, "%s\tr%d, r%d, r%d",
+        r_lookup[R_TYPE(instr)],
+        R_DST(instr),
+        R_SRC1(instr),
+        R_SRC2(instr));
+      }
+      return;
+    }
+    case RFE: {
+      sprintf(res, "%s", 
+        lookup[TYPE(instr)]);
+      return;
+    }
+    case J:
+    case JAL: {
+      sprintf(res, "%s\t%d", 
+        lookup[TYPE(instr)],
+        L_SGN(L_IMM(instr)));
+      return;
+    }
+    case TRAP: {
+      sprintf(res, "%s\t%u", 
+        lookup[TYPE(instr)],
+        L_IMM(instr));
+      return;
+    }
+    case LB:
+    case LBU:
+    case LH:
+    case LHU:
+    case LW: {
+      sprintf(res, "%s\tr%d, %d(r%d)",
+        lookup[TYPE(instr)],
+        I_DST(instr),
+        I_SGN(I_IMM(instr)),
+        I_SRC(instr));
+      return;
+    }
+    case SB:
+    case SH:
+    case SW: {
+      sprintf(res, "%s\t%d(r%d), r%d",
+        lookup[TYPE(instr)],
+        I_SGN(I_IMM(instr)),
+        I_DST(instr),
+        I_SRC(instr));
+      return;
+    }
+    case LHI:
+    case BEQZ:
+    case BNEZ: {
+      sprintf(res, "%s\tr%d, %.4x",
+        lookup[TYPE(instr)],
+        I_DST(instr),
+        I_IMM(instr));
+      return;
+    }
+    case JR:
+    case JALR: {
+      sprintf(res, "%s\tr%d",
+        lookup[TYPE(instr)],
+        I_SRC(instr));
+      return;
+    }
+    default: {
+      char *s = lookup[TYPE(instr)];
+      if (s == 0) {
+        sprintf(res, "%s", "UNKNOWN");
+      } else if (s[strlen(s)-2] == 'U') {
+        sprintf(res, "%s\tr%d, r%d, %u",
+          s, I_DST(instr),
+          I_SRC(instr),
+          I_IMM(instr));
+      } else {
+        sprintf(res, "%s\tr%d, r%d, %d",
+          s, I_DST(instr),
+          I_SRC(instr),
+          I_SGN(I_IMM(instr)));
+      }
+      return;
+    }
+  }
+}
 
 #define I(tab, instr) \
-  do { \
-    (tab[instr]) = #instr; \
-  } while(0);
-
-char **disassemble(word_t *instr, int sz)
+do { \
+  (tab[instr]) = #instr; \
+} while(0);
+// Create a table for looking up R-type instructions.
+void create_r_table(char **r_lookup)
 {
-  int i;
-  char *lookup[1<<6];
-  char *r_lookup[1<<6];
-  for (i = 0; i < (1<<6); i++) {
-    lookup[i] = 0;
-    r_lookup[i] = 0;
-  }
-  char *special_reg_lookup[8] = {
-    "UNKNOWN",
-    "PSW",
-    "XAR",
-    "XBR",
-    "MBR",
-    "MLR",
-    "TSR",
-    "TCR"
-  };
-
   // R instructions
   I(r_lookup, NOP);
   I(r_lookup, HALT);
@@ -63,7 +155,11 @@ char **disassemble(word_t *instr, int sz)
   I(r_lookup, SGE);
   I(r_lookup, MOVI2S);
   I(r_lookup, MOVS2I);
+}
 
+// Create a table for looking up other instructions.
+void create_table(char **lookup)
+{
   // I instructions
   I(lookup, ADDI);
   I(lookup, ADDUI);
@@ -107,120 +203,38 @@ char **disassemble(word_t *instr, int sz)
   I(lookup, JAL);
   I(lookup, RFE);
   I(lookup, TRAP);
+}
+
+#undef I
+
+// Disassembles the array of sz instructions at instr.
+// Caller must free the returned array.
+char **disassemble(word_t *instr, int sz)
+{
+  int i;
+  char *lookup[1<<6];
+  char *r_lookup[1<<6];
+  for (i = 0; i < (1<<6); i++) {
+    lookup[i] = 0;
+    r_lookup[i] = 0;
+  }
+  char *special_reg_lookup[32] = {
+    "???", "PSW", "XAR", "XBR",
+    "MBR", "MLR", "TSR", "TCR",
+    "???", "???", "???", "???",
+    "???", "???", "???", "???",
+    "???", "???", "???", "???",
+    "???", "???", "???", "???",
+    "???", "???", "???", "???",
+    "???", "???", "???", "???"
+  };
+  create_r_table(r_lookup);
+  create_table(lookup);
 
   char **res = (char**) malloc(sizeof(char*) * sz);
   for (i = 0; i < sz; i++) {
     res[i] = (char*) malloc(COMMAND_LEN);
-    switch(TYPE(instr[i])) {
-      case 0: {
-        if (r_lookup[R_TYPE(instr[i])] == 0) {
-          sprintf(res[i], "%s", "UNKNOWN");
-        } else if (instr[i] == MOVS2I) {
-          if (r_lookup[R_TYPE(instr[i])] == 0) {
-            sprintf(res[i], "%s", "UNKNOWN");
-          } else {
-            sprintf(res[i], "%s\t%d, %s",
-              lookup[R_TYPE(instr[i])],
-              R_DST(instr[i]),
-              special_reg_lookup[R_SRC1(instr[i])]);
-          }
-        } else if (instr[i] == MOVI2S) {
-          if (R_DST(instr[i]) > 8) {
-            sprintf(res[i], "%s", "UNKNOWN");
-          } else {
-            sprintf(res[i], "%s\t%s, %d",
-              lookup[R_TYPE(instr[i])],
-              special_reg_lookup[R_DST(instr[i])],
-              R_SRC1(instr[i]));
-          }
-        } else if (instr[i] == NOP ||
-            instr[i] == HALT ||
-            instr[i] == WAIT) {
-          sprintf(res[i], "%s", 
-            r_lookup[R_TYPE(instr[i])]);
-        } else {
-          sprintf(res[i], "%s\tr%d, r%d, r%d",
-          r_lookup[R_TYPE(instr[i])],
-          R_DST(instr[i]),
-          R_SRC1(instr[i]),
-          R_SRC2(instr[i]));
-        }
-        break;
-      }
-      case RFE: {
-        sprintf(res[i], "%s", 
-          lookup[TYPE(instr[i])]);
-        break;
-      }
-      case J:
-      case JAL: {
-        sprintf(res[i], "%s\t%d", 
-          lookup[TYPE(instr[i])],
-          L_SGN(L_IMM(instr[i])));
-        break;
-      }
-      case TRAP: {
-        sprintf(res[i], "%s\t%u", 
-          lookup[TYPE(instr[i])],
-          L_IMM(instr[i]));
-        break;
-      }
-      case LB:
-      case LBU:
-      case LH:
-      case LHU:
-      case LW: {
-        sprintf(res[i], "%s\tr%d, %d(r%d)",
-          lookup[TYPE(instr[i])],
-          I_DST(instr[i]),
-          I_SGN(I_IMM(instr[i])),
-          I_SRC(instr[i]));
-        break;
-      }
-      case SB:
-      case SH:
-      case SW: {
-        sprintf(res[i], "%s\t%d(r%d), r%d",
-          lookup[TYPE(instr[i])],
-          I_SGN(I_IMM(instr[i])),
-          I_DST(instr[i]),
-          I_SRC(instr[i]));
-        break;
-      }
-      case LHI:
-      case BEQZ:
-      case BNEZ: {
-        sprintf(res[i], "%s\tr%d, %.4x",
-          lookup[TYPE(instr[i])],
-          I_DST(instr[i]),
-          I_IMM(instr[i]));
-        break;
-      }
-      case JR:
-      case JALR: {
-        sprintf(res[i], "%s\tr%d",
-          lookup[TYPE(instr[i])],
-          I_SRC(instr[i]));
-        break;
-      }
-      default: {
-        char *s = lookup[TYPE(instr[i])];
-        if (s == 0) {
-          sprintf(res[i], "%s", "UNKNOWN");
-        } else if (s[strlen(s)-1] == 'U') {
-          sprintf(res[i], "%s\tr%d, r%d, %u",
-            s, I_DST(instr[i]),
-            I_SRC(instr[i]),
-            I_IMM(instr[i]));
-        } else {
-          sprintf(res[i], "%s\tr%d, r%d, %d",
-            s, I_DST(instr[i]),
-            I_SRC(instr[i]),
-            I_SGN(I_IMM(instr[i])));
-        }
-        break;
-      }
-    }
+    parse_instr(lookup, r_lookup, special_reg_lookup, instr[i], res[i]);
   }
   return res;
 }
